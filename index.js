46da0e4e63e99868ba1e9c901d82fa9198481cb3
everstrive <gabrielleconway12345@gmail.com>
2018-10-06 12:42:59 -0700

    Updated website message for android

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ var api = new ParseServer({
 	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
 	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
 	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-	
+
 	//**** Security Settings ****//
-	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false, 
+	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
 	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!	
-	
+	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
+
 	//**** Live Query ****//
 	// liveQuery: {
 	// 	classNames: ["TestObject", "Place", "Team", "Player", "ChatMessage"] // List of classes to support for query subscriptions
@@ -34,7 +34,7 @@ var api = new ParseServer({
 	/* Set the mount path as it is in serverURL */
 	// publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
 	/* This will appear in the subject and body of the emails that are sent */
-	// appName: process.env.APP_NAME || "CodeCraft", 
+	// appName: process.env.APP_NAME || "CodeCraft",
 
 	// emailAdapter: {
 	// 	module: 'parse-server-simple-mailgun-adapter',
@@ -44,7 +44,7 @@ var api = new ParseServer({
 	// 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
 	// 	}
 	// },
-	
+
 	//**** File Storage ****//
 	// filesAdapter: new S3Adapter(
 	// 	{
@@ -67,7 +67,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
-	res.status(200).send('I dream of being a website and other fun things!');
+	res.status(200).send('I dream of logging into this website with Android!');
 });
 
 // There will be a test page available on the /test path of your server url
